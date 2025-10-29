4038654aa22b6d8ebb03c34b3a7fb5f77a51fc58
Adarsh Sugandhe <adarshsugandhe2024@gmail.com>
2025-10-29 21:25:39 +0530

    Updated font size for the heading in the Journey component

--- components/Journey.jsx
+++ components/Journey.jsx
@@ -7,7 +7,7 @@ const Journey = () => {
   return (
     <section id="journey" className="bg-black px-2 md:px-4 pt-10">
       <div className="container mx-auto">
-        <Heading heading="MY PROFESSIONAL JOURNEY" styles="h2 font-bold text-center text-white text-6xl md:text-8xl" />
+        <Heading heading="MY PROFESSIONAL JOURNEY" styles="h2 font-bold text-center text-white text-[55px] md:text-8xl" />
         <Cards />
       </div>
     </section>

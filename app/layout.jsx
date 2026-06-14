9b29cc2c2e0e41ada2ac8fe33579e09e02eedd79
AdarshSugandhe <adarshsugandhe2024@gmail.com>
2026-06-14 18:51:39 +0530

    gets GA id from .env in layout file

--- app/layout.jsx
+++ app/layout.jsx
@@ -39,7 +39,7 @@ export default function RootLayout({ children }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
                 page_path: window.location.pathname,
               });
             `,
